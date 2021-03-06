515a1c3a8a5d77fa3b14ae7776886ce3db7da1bc
Jalpesh <jalpesh.patel1@gmail.com>
2021-03-06 18:38:49 +1100

    some planning on how to approach a problem

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+// Step 1 - Get the input csv path from console & get the data of csv
+            // If valid path not found (needs some action) might take test_data.csv as a fallback data.
+
+// Step 2 - Get the input value from the console
+            // Check for the valid input if proper input not provided then show message to provide valid input.
+            // Also, need to make sure it does not exit the program once it returns the output untill user exit the console.
+
+// Step 3 - Find/Calulate the travel time between device using valid from and to value along with Time
+          // Set all the unique Devices(from and to) i.e in our case (A, B, C, D...).(Bascially it will be dynamic)
+          // Create a object/array in such a way that it contains all the properties such as 
+          // adjacent devices with its latency
+          // Set the input device from and to which is provided from the user input
+          
